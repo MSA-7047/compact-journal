520a5642238bfae6eecdc0aad71d61a6c9f18367
Adil-Kassam <kassamadil786@gmail.com>
2024-03-27 08:47:06 +0000

    Fix errors in test journal model

--- journal/tests/models/test_journal_model.py
+++ journal/tests/models/test_journal_model.py
@@ -1,6 +1,9 @@
 from django.test import TestCase
 from django.contrib.auth.models import User
 from journal.models import User, Journal
+from django.core.exceptions import ValidationError
+from journal.forms import CreateJournalForm
+
 
 class JournalTestCase(TestCase):
     def setUp(self):
@@ -39,15 +42,41 @@ class JournalTestCase(TestCase):
 
     def test_invalid_journal_title_max_length(self):
         # Try to create a Journal instance with a title exceeding max length
-        with self.assertRaises(ValueError):
-            invalid_title_journal = self.journal.journal_title = 'A' * 51  # Exceeds max length
-            invalid_title_journal.full_clean()  # This should raise a ValueError
+        with self.assertRaises(ValidationError) as context:
+            invalid_title_journal = Journal(
+                journal_title='A' * 51 , # Exceeds max length
+                journal_description='This is a valid description.',
+                journal_bio='This is a valid bio.',
+                journal_mood='Happy',
+                journal_owner=self.user,
+                private=True
+            )
+            invalid_title_journal.full_clean()
+
+        # Check if the ValidationError contains the correct error message
+        self.assertIn('Ensure this value has at most 50 characters', str(context.exception))
+
+
 
     def test_invalid_journal_title_empty(self):
         # Try to create a Journal instance with an empty title
-        with self.assertRaises(ValueError):
-            empty_title_journal = self.journal.journal_title = ''
-            empty_title_journal.full_clean()  # This should raise a ValueError
+        form_input = {
+            'journal_title': '',
+            'journal_description': 'B' * 1000,
+            'journal_bio': 'C' * 10000,
+            'journal_mood': 'Happy',
+            'journal_owner': self.user,
+            'private': True
+        }
+        
+        # Instantiate the form with the provided data
+        form = CreateJournalForm(data=form_input)
+        
+        # Assert that the form is not valid
+        self.assertFalse(form.is_valid())
+        
+        # Assert that the correct error message is present in non-field errors
+        self.assertEqual(form.errors['journal_title'], ['This field is required.'])
             
     def test_valid_journal_description(self):
         # Create a Journal instance with a valid description
@@ -61,34 +90,56 @@ class JournalTestCase(TestCase):
         )
         valid_description_journal.full_clean()  # Should not raise any errors
 
+        # Save the instance to the database
+        valid_description_journal.save()
+
         # Check if the instance is saved successfully
         self.assertIsNotNone(valid_description_journal.pk)
 
-    def test_invalid_journal_description_max_length(self):
-        # Try to create a Journal instance with a description exceeding max length
-        with self.assertRaises(ValueError):
-            invalid_description_journal = Journal(
-                journal_title='Invalid Description Journal',
-                journal_description='D' * 1001,  # Exceeds max length
-                journal_bio='This is a valid bio.',
-                journal_mood='Happy',
-                journal_owner=self.user,
-                private=True
-            )
-            invalid_description_journal.full_clean()  # This should raise a ValueError
+
+    def test_journal_description_max_length(self):
+        # Prepare form input with a description exceeding the maximum length
+        form_input = {
+            'journal_title': 'A' * 50,
+            'journal_description': 'D' * 1001,  # Exceeds max length
+            'journal_bio': 'This is a valid bio.',  
+            'journal_mood': 'Happy',
+            'journal_owner': self.user,
+            'private': True
+        }
+        
+        # Instantiate the form with the provided data
+        form = CreateJournalForm(data=form_input)
+        
+        # Assert that the form is not valid
+        self.assertFalse(form.is_valid())
+        
+        # Assert that the correct error message is present in non-field errors
+        self.assertEqual(form.errors['journal_description'], ['Ensure this value has at most 1000 characters (it has 1001).'])
+            
 
     def test_invalid_journal_description_empty(self):
         # Try to create a Journal instance with an empty description
-        with self.assertRaises(ValueError):
-            empty_description_journal = Journal(
-                journal_title='Empty Description Journal',
-                journal_description='',
-                journal_bio='This is a valid bio.',
-                journal_mood='Happy',
-                journal_owner=self.user,
-                private=True
-            )
-            empty_description_journal.full_clean()  # This should raise a ValueError
+
+        form_input = {
+            'journal_title': 'Test Title',
+            'journal_description': '' ,
+            'journal_bio': 'C' * 10000,
+            'journal_mood': 'Happy',
+            'journal_owner': self.user,
+            'private': True
+        }
+        
+        # Instantiate the form with the provided data
+        form = CreateJournalForm(data=form_input)
+        
+        # Assert that the form is not valid
+        self.assertFalse(form.is_valid())
+        
+        # Assert that the correct error message is present in non-field errors
+        self.assertEqual(form.errors['journal_description'], ['This field is required.'])
+
+
     def test_valid_journal_bio(self):
         # Create a Journal instance with a valid bio
         valid_bio_journal = Journal(
@@ -100,32 +151,29 @@ class JournalTestCase(TestCase):
             private=True
         )
         valid_bio_journal.full_clean()  # Should not raise any errors
+        
+        valid_bio_journal.save()
 
         # Check if the instance is saved successfully
         self.assertIsNotNone(valid_bio_journal.pk)
 
-    def test_invalid_journal_bio_max_length(self):
-        # Try to create a Journal instance with a bio exceeding max length
-        with self.assertRaises(ValueError):
-            invalid_bio_journal = Journal(
-                journal_title='Invalid Bio Journal',
-                journal_description='This is a valid description.',
-                journal_bio='B' * 10001,  # Exceeds max length
-                journal_mood='Happy',
-                journal_owner=self.user,
-                private=True
-            )
-            invalid_bio_journal.full_clean()  # This should raise a ValueError
+    def test_journal_bio_max_length(self):
+        # Prepare form input with a description exceeding the maximum length
+        form_input = {
+            'journal_title': 'A' * 50,
+            'journal_description': 'B' * 1000,
+            'journal_bio': 'C' * 10001,  # Exceeds max length
+            'journal_mood': 'Happy',
+            'journal_owner': self.user,
+            'private': True
+        }
+        
+        # Instantiate the form with the provided data
+        form = CreateJournalForm(data=form_input)
+        
+        # Assert that the form is not valid
+        self.assertFalse(form.is_valid())
+        
+        # Assert that the correct error message is present in non-field errors
+        self.assertEqual(form.errors['journal_bio'], ['Ensure this value has at most 10000 characters (it has 10001).'])
 
-    def test_invalid_journal_bio_empty(self):
-        # Try to create a Journal instance with an empty bio
-        with self.assertRaises(ValueError):
-            empty_bio_journal = Journal(
-                journal_title='Empty Bio Journal',
-                journal_description='This is a valid description.',
-                journal_bio='',
-                journal_mood='Happy',
-                journal_owner=self.user,
-                private=True
-            )
-            empty_bio_journal.full_clean()  # This should raise a ValueError
