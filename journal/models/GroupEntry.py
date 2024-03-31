da49c0dd0869833b6900a9f6986374ab74ecaec2
javed-hussain <j.hussain1204@gmail.com>
2024-03-31 05:34:07 +0100

    re add changes

--- journal/models/GroupEntry.py
+++ journal/models/GroupEntry.py
@@ -7,18 +7,7 @@ from django_ckeditor_5.fields import CKEditor5Field
 
 class GroupEntry(AbstractEntry.AbstractEntry):
     """Model to represent the Journals the Group will be writing on."""
-    journal_title = models.CharField(max_length=50, blank=False)
-    journal_description = models.TextField(max_length=1000, blank=False)
-    journal_bio = CKEditor5Field('Entry', config_name='extends', max_length=10000)
-    entry_date = models.DateTimeField(auto_now_add=True)
     last_edited_by = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True, blank=True)
-    MOOD_OPTIONS = [
-        ("Sad", "Sad"),
-        ("Happy", "Happy"),
-        ("Angry", "Angry"),
-        ("Neutral", "Neutral")
-    ]
-    journal_mood = models.CharField(choices=MOOD_OPTIONS, blank=False, max_length=7)
     owner = models.ForeignKey(Group, on_delete=models.CASCADE)
 
     class Meta:
