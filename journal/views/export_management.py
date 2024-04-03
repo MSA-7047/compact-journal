de08819326af734e74fe8aa28b38f716f36c882c
javed-hussain <j.hussain1204@gmail.com>
2024-04-03 17:11:08 +0100

    commit changes

--- journal/views/export_management.py
+++ journal/views/export_management.py
@@ -22,45 +22,51 @@ def export_single_entry_as_PDF(request, entry_id):
         messages.warning(request, "You have attempted to export an entry that is not yours, redirected to dashboard")
         return redirect(reverse('dashboard'))
 
-    template = get_template('entry_as_PDF.html')
-    html = template.render({"entry": entry_instance})  # Pass context data if needed
-    title = entry_instance.title
+    return export_to_pdf(
+        template_src='entry_as_PDF.html',
+        context_dict={"entry": entry_instance},
+        title=entry_instance.title
+    )
+
+def export_journal_as_PDF(request, journal_entries):
+
+    journal_entries_list, journal = unpack_journals(journal_entries)
+
+    return export_to_pdf(
+        template_src='journal_as_PDF.html',
+        context_dict={"journal_entries": journal_entries_list, "journal": journal},
+        title=f'{journal.title} entries'
+    )
+
+def export_single_group_entry_as_PDF(request, group_id, journal_id):
+    entry = GroupEntry.objects.get(id=journal_id)
+    group_ = get_object_or_404(Group, group_id=group_id)
+    return export_to_pdf(
+        template_src='group_entry_as_PDF.html',
+        context_dict={"group": group_, "entry": entry},
+        title=f'{group_.name}_{entry.title}'
+    )
+
+def export_group_journal_as_PDF(request, group_id):
+    group_ = get_object_or_404(Group, group_id=group_id)
+    journal_entries = GroupEntry.objects.filter(owner=group_)
+    return export_to_pdf(
+        template_src='group_journal_as_PDF.html',
+        context_dict={"journal_entries": journal_entries,"group": group_},
+        title=f'{group_.name}_journals')
+    
+def export_to_pdf(template_src, context_dict,title):
+    template = get_template(template_src)
+    html = template.render(context_dict)
     response = HttpResponse(content_type='application/pdf')
     response['Content-Disposition'] = f'attachment; filename={title}.pdf'
     pisa.CreatePDF(html, dest=response)
     return response
 
-def export_journal_as_PDF(request, journal_entries):
-
+def unpack_journals(journal_entries):
     journals = journal_entries.split(',')
     journal_entries = []
     for journal in journals:
         journal_entries.append(get_object_or_404(Entry, id=int(journal)))
     journal = journal_entries[0].journal
-    template = get_template('journal_as_PDF.html')
-    html = template.render({"journal_entries": journal_entries, "journal": journal}) 
-    title = journal.title  + " entries"
-    response = HttpResponse(content_type='application/pdf')
-    response['Content-Disposition'] = f'attachment; filename={title}.pdf'
-    pisa.CreatePDF(html, dest=response)
-    return response
-
-def export_single_group_entry_as_PDF(request, group_id, journal_id):
-    entry = GroupEntry.objects.get(id=journal_id)
-    group_ = get_object_or_404(Group, group_id=group_id)
-    template = get_template('group_entry_as_PDF.html')
-    html = template.render({"group": group_, "entry": entry})
-    response = HttpResponse(content_type='application/pdf')
-    response['Content-Disposition'] = f'attachment; filename={group_.name}_{entry.title}.pdf'
-    pisa.CreatePDF(html, dest=response)
-    return response
-
-def export_group_journal_as_PDF(request, group_id):
-    group_ = get_object_or_404(Group, group_id=group_id)
-    journal_entries = GroupEntry.objects.filter(owner=group_)
-    template = get_template('group_journal_as_PDF.html')
-    html = template.render({"journal_entries": journal_entries, "group": group_})
-    response = HttpResponse(content_type='application/pdf')
-    response['Content-Disposition'] = f'attachment; filename={group_.name}_journals .pdf'
-    pisa.CreatePDF(html, dest=response)
-    return response
+    return journal_entries, journal
