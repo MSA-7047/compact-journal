b76ed8a197e20dfe8c59cb9648e14e59ae1093d4
Miqdaad Al-Hassan <mm.al_hassan@outlook.com>
2024-04-03 15:55:58 +0100

    Fix Seed & Complete tests
    All tests now pass
    
    Also unwanted files are now deleted

--- journal/models/AbstractEntry.py
+++ journal/models/AbstractEntry.py
@@ -7,7 +7,7 @@ from django_ckeditor_5.fields import CKEditor5Field
 
 class AbstractEntry(models.Model):
     title = models.CharField('Title', max_length=30, blank=False)
-    summary = models.TextField('Summary', max_length=200)
+    summary = models.CharField('Summary', max_length=200)
     content = CKEditor5Field('Entry Content', config_name='extends', max_length=10000)
     entry_date = models.DateTimeField(auto_now_add=True)
     last_edited = models.TimeField(auto_now=True)
