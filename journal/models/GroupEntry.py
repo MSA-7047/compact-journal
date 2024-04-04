f7402f76eb7cc42444deb7ef41fc73afdd1d729c
Rahat Hussain <md.hussain@kcl.ac.uk>
2024-04-04 02:32:32 +0100

    Remove import of ckeditor from groupentry

--- journal/models/GroupEntry.py
+++ journal/models/GroupEntry.py
@@ -3,7 +3,6 @@ from django.db import models
 from journal.models import AbstractEntry, Group
 
 
-
 class GroupEntry(AbstractEntry.AbstractEntry):
     """Model to represent the Journals the Group will be writing on."""
     last_edited_by = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True, blank=True)
